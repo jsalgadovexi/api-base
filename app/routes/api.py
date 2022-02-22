c950becec6b26f78ff1e99baa23b24f8ebe0f78f
jsalgadovexi <90008678+jsalgadovexi@users.noreply.github.com>
2022-02-22 11:15:48 -0600

    Meet viernes, 18 de febrero
    Objetivo: Agregar el endpoint a las rutas de la API. Y probar el nuevo servicio creado mediante la documentación (Swagger) o postman. Revisar que se inserte correctamente en BD y devuelva un response.

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from routes import controller
+from routes import controller, solicitud
 
 api_router = APIRouter()
 api_router.include_router(controller.router, tags=["pruebas"], prefix="/base")
+api_router.include_router(solicitud.router, tags=["originación"], prefix="/solicitud")
